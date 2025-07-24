bfbdb6811f8a7243327531c170d40a96b472a896
MNihal7961 <mnihal7961@gmail.com>
2025-07-24 19:16:08 +0530

    Add layout components: Header, Footer, and Layout for consistent page structure

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import React, { type ReactNode } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <section className="max-w-[1600px] flex flex-col items-start justify-center mx-auto p-14">
+          {children}
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
