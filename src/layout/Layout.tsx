50d1a5bef9a2787e26d5ee4e8916df1239481b00
MNihal7961 <mnihal7961@gmail.com>
2025-07-24 19:46:50 +0530

    Remove padding from Layout component section for improved spacing consistency

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-1">
-        <section className="max-w-[1600px] flex flex-col items-start justify-center mx-auto p-14">
+        <section className="max-w-[1600px] flex flex-col items-start justify-center mx-auto">
           {children}
         </section>
       </main>
